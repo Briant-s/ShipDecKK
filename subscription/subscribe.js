9587080a8a9f3017ab5587e611305cd1b7097ae6
Briant--S <briantsandriano@gmail.com>
2025-05-20 19:23:01 +0700

    subscribe 100%

--- subscription/subscribe.js
+++ subscription/subscribe.js
@@ -9,6 +9,7 @@ const phone_input = document.getElementById("phone");
 const password_input = document.getElementById("password");
 const con_password_input = document.getElementById("confirm-pass");
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phoneRegex = /^\+[0-9]{1}-[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
 
 
 // error message values
@@ -46,6 +47,17 @@ form.addEventListener("submit", (e) => {
         email_input.previousElementSibling.classList.add("invalid");
         email_input.classList.add("form-invalid");
     }
+    else if (!emailRegex.test(email)){
+        errors++;
+        error_email.innerText = "Email Format Must be Correct";
+        error_email.parentElement.style.display = "inline-flex";
+        email_input.previousElementSibling.classList.add("invalid");
+        email_input.classList.add("form-invalid");
+    }
+    else if (emailRegex.test(email)){
+        error_email.parentElement.style.display = "none";
+        email_input.classList.add("form-valid");
+    }
     
     // handle phone number
     if (phone === "" || phone === null){
@@ -54,6 +66,17 @@ form.addEventListener("submit", (e) => {
         error_phone.parentElement.style.display = "inline-flex";
         phone_input.previousElementSibling.classList.add("invalid");
         phone_input.classList.add("form-invalid");
+    }
+    else if (!phoneRegex.test(phone)){
+        errors++;
+        error_phone.innerText = "Phone Number Format Must be Correct";
+        error_phone.parentElement.style.display = "inline-flex";
+        phone_input.previousElementSibling.classList.add("invalid");
+        phone_input.classList.add("form-invalid");
+    }
+    else if (phoneRegex.test(phone)){
+        error_phone.parentElement.style.display = "none";
+        phone_input.classList.add("form-valid");
     }
     
     // handle password
@@ -71,6 +94,10 @@ form.addEventListener("submit", (e) => {
         password_input.previousElementSibling.classList.add("invalid");
         password_input.classList.add("form-invalid");
     }
+    else if (password.length >= 8){
+        error_password.parentElement.style.display = "none";
+        password_input.classList.add("form-valid");
+    }
     
     // handle password
     if (con_password === "" || con_password === null){
@@ -87,9 +114,16 @@ form.addEventListener("submit", (e) => {
         con_password_input.previousElementSibling.classList.add("invalid");
         con_password_input.classList.add("form-invalid");
     }
+    else if (con_password === password){
+        error_con_password.parentElement.style.display = "none";
+        con_password_input.classList.add("form-valid");
+    }
 
     if (errors > 0){
         e.preventDefault();
+    }
+    else if (errors === 0){
+        alert("Account Created!");
     }
 
 })
